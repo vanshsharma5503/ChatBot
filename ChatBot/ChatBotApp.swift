f6b49c9e5c5f338b5d24b1be0c06bc39e8a45466
vansh sharma <vb2810@srmist.edu.in>
2024-03-19 15:09:45 +0530

    Initial Commit

---
+++ ChatBot/ChatBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotApp.swift
+//  ChatBot
+//
+//  Created by SHHH!! private on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
